da2f3a87834569127132f18c3176c7cb9d677604
Oscar A Castro <oacastro2@gmail.com>
2020-09-09 14:32:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -39,7 +39,7 @@ def get_japanese_emoticon(file, emoji)
       emoticon_lib[key][:english] == emoji
     #binding.pry 
       end
-        found_emoji ? emoticon_lib[:japanese] : "Sorry, that emoticon was not found"
+        found_emoji ? found_emoji = emoticon_lib[:japanese] : "Sorry, that emoticon was not found"
   #binding.pry 
       end 
       
